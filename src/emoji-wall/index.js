49d5a6d26b558f7a94376cd985fc5784fce9752f
Pierre Reimertz <reimertz@me.com>
2021-06-30 13:01:16 +0200

    fix model folder path for netlify build

--- src/emoji-wall/index.js
+++ src/emoji-wall/index.js
@@ -1,16 +1,16 @@
-
 import * as tf from '@tensorflow/tfjs'
-import * as faceapi from 'face-api.js';
+import * as faceapi from 'face-api.js'
 
-import { byId, initiateWebcam }  from '../utils'
-import { getAPI }  from '../firebase'
+import { byId, initiateWebcam } from '../utils'
+import { getAPI } from '../firebase'
 
 function expressionsToEmoji(expressions) {
-  const [emotion, probability] = Object.entries(expressions)
-    .sort(([emotion1, prob1], [emotion2, prob2])  => prob2 - prob1)
-    .filter(([emotion, prob]) => prob >= 0.7)[0] || []
+  const [emotion, probability] =
+    Object.entries(expressions)
+      .sort(([emotion1, prob1], [emotion2, prob2]) => prob2 - prob1)
+      .filter(([emotion, prob]) => prob >= 0.7)[0] || []
 
-  switch(emotion) {
+  switch (emotion) {
     case 'neutral':
       return '😐'
     case 'happy':
@@ -31,42 +31,42 @@ function expressionsToEmoji(expressions) {
 }
 
 const robot = {
-  api: null, 
+  api: null,
   eyes: byId('webcam'),
   mood: '🤖',
 
-  wakeUp: async function() {
-    try { 
-      await initiateWebcam(robot.eyes) 
+  wakeUp: async function () {
+    try {
+      await initiateWebcam(robot.eyes)
+    } catch (e) {
+      alert(`Couldn't access your camera 😢`)
     }
-    catch (e) {
-      alert(`Couldn't access your camera 😢`);
-    }
-    
-    await faceapi.nets.ssdMobilenetv1.load('models/face-api')
-    await faceapi.nets.faceExpressionNet.load('models/face-api')
+
+    await faceapi.nets.ssdMobilenetv1.load('./models/face-api')
+    await faceapi.nets.faceExpressionNet.load('./models/face-api')
 
     robot.api = await getAPI()
     robot.work()
   },
 
-  work: async function() {
+  work: async function () {
     try {
-      const { expressions } = await faceapi.detectSingleFace(robot.eyes).withFaceExpressions()
+      const { expressions } = await faceapi
+        .detectSingleFace(robot.eyes)
+        .withFaceExpressions()
       const oldEmoji = robot.mood
-      robot.mood = (expressionsToEmoji(expressions) || robot.mood)
-       
+      robot.mood = expressionsToEmoji(expressions) || robot.mood
+
       if (robot.mood !== oldEmoji) {
         byId('emoji').textContent = robot.mood
         robot.api.setEmoji(robot.mood)
       }
-
+    } catch (e) {
+      console.log(e)
     }
-    catch (e) { console.log(e) }
 
     window.requestAnimationFrame(() => robot.work())
-  }
+  },
 }
 
 robot.wakeUp()
-
